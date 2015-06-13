ead6cdac1bb02594e5e02861359a651be268430f
Shamil Pirumow <c_tiger@bk.ru>
2015-06-13 21:08:53 +0700

    v 0.1 Первый коммит

---
+++ crazy-social.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: crazy-social
+Plugin URI: http://prmv.ru/
+Description: Социальные кнопки
+Version: 0.1
+Author: Shamil Pirumow
+Author URI: http://prmv.ru/
+*/
+function insertCrazySocial($content) {
+	if(is_single() || is_page() && !is_home() && !is_front_page() ) {
+		$content.='<div class="crazy-social"><script type="text/javascript" src="//yandex.st/share/share.js" charset="utf-8"></script> <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="button" data-yashareQuickServices="yaru,vkontakte,facebook,twitter,odnoklassniki,moimir,lj,gplus,surfingbird"></div></div>';
+	}
+	return $content;
+}
+
+add_filter('the_content', 'insertCrazySocial');
+
+?>
