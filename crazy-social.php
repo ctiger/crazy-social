25e1549dca2164134088272449b47904d5593d2a
Shamil Pirumow <c_tiger@bk.ru>
2015-06-13 21:27:48 +0700

    v 0.2 Изменения в API Яндекса

--- crazy-social.php
+++ crazy-social.php
@@ -9,7 +9,7 @@ Author URI: http://prmv.ru/
 */
 function insertCrazySocial($content) {
 	if(is_single() || is_page() && !is_home() && !is_front_page() ) {
-		$content.='<div class="crazy-social"><script type="text/javascript" src="//yandex.st/share/share.js" charset="utf-8"></script> <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="button" data-yashareQuickServices="yaru,vkontakte,facebook,twitter,odnoklassniki,moimir,lj,gplus,surfingbird"></div></div>';
+        $content.='<div class="crazy-social"><script type="text/javascript" src="//yastatic.net/share/share.js" charset="utf-8"></script><div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="button" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,lj,gplus"></div>';
 	}
 	return $content;
 }
